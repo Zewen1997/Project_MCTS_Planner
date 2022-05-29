af1ea204925bcfb09b29f3702fe62616379b06d2
Zewen1997 <chenzewen1997@gmail.com>
2022-05-29 20:38:23 +0200

    python's instantiated objects can only communicate exterally and cannot be transferred to c++

--- test/test_MCTS_Planner.py
+++ test/test_MCTS_Planner.py
@@ -3,18 +3,12 @@ import MCTS
 
 print("-------------------- test MCTS lib --------------------")
 
-reference_path = [0, 0.5, 0.7]
+reference_path = [0, 0.1835, 0.4]
 waypoint1 = MCTS.Waypoint(0, 0, 0, 0.9, 0)
-waypoint2 = MCTS.Waypoint(0.4, 0.3, 0.5, 0.88, 0)
-waypoint3 = MCTS.Waypoint(0.5, 0.32, 0.7, 0.88, 0)
+waypoint2 = MCTS.Waypoint(0.4, 0.3, 0.1835, 0.88, 0)
+waypoint3 = MCTS.Waypoint(0.5, 0.32, 0.4, 0.88, 0)
 waypoint_ego = [waypoint1, waypoint2, waypoint3]
 
-
-
-
-# MCTS Interface
-mcts_interface = MCTS.MCTSInterface()
-print(mcts_interface)
 
 # MCTS EgoState
 ego_state = MCTS.EgoState(0.16, 0.25, 0, 0.95, 5, 1.8, 5)
@@ -34,22 +28,25 @@ state_status = MCTS.StatesStatus(False, False)
 mcts_state = MCTS.EnvironmentState(ego_state, objects_state,state_status)
 mcts_state.print()
 
-# set EnvironmentState with interface
-mcts_interface.setEnvironmentState(mcts_state)
 
 # MCTS MCTSPlanner 
 mcts_planner = MCTS.MCTSPlanner()
 print("done init planner", mcts_planner)
 
-mcts_planner.interface().setEnvironmentState(mcts_state)
-
 # MCTS process functions
+print("init:")
 mcts_planner.processInit()
-mcts_planner.processRun()
+print("set state:")
+mcts_planner.setDecisions([1.0,5.0,9.0])
+print(mcts_planner.getDecisions())
+# mcts_planner.processRun()
 
-# MCTS MCTSPlanner output through interface
-decisions = mcts_planner.interface().getDecisions()
-print("decisions ", decisions)
-
-data_handler = MCTS.DataHandler()
-print(data_handler.getNearstVehicles(ego_state, objects_state))
+# Test update states funtion
+mcts = MCTS.MCTSState()
+mcts.createStartState(mcts_state)
+mcts.setReferencePathEgo(reference_path)
+mcts.setWaypointEgo(waypoint_ego)
+action = MCTS.ActionType(2)
+print(action)
+next_state = mcts.calculateNextEgoState(mcts_state, action)
+next_state.print()
